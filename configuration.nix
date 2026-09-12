4d6fa464f2cdd7d85baccba6d43d1651e2b996df
Vladislav <IsaeVladddd@yandex.com>
2026-09-12 11:39:40 +0500

    todo: fix sound

--- configuration.nix
+++ configuration.nix
@@ -52,8 +52,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # Включить niri
+  services.displayManager.ly.enable = true;
+  programs.niri.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
